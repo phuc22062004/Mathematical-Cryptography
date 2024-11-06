46ea75511a5f4e099b2df2adbaccac5067264954
phuc22062004 <anhphucx22@gmail.com>
2024-11-07 01:25:18 +0700

    init

---
+++ project_01_source/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// A: Trien khai ham luy thua mo-dun
+//  <...> modular_exponentiation(<..> base, <..> exponent, <..> mod){
+
+// }
+
+// B: Trien khai ham sinh so nguyen to ngau nhien
+//<..> generate_safe_prime(int bit_size){
+
+// }
+
+// C: Trien khai ham sinh khoa ngau nhien
+//<..> generate_private_key(<..> p){
+//  }
+
+// D: Hoan thanh logic trao doi khoa Diffie-Hellman
+// int main(){
+// 1. Sinh so nguyen to lon hon p va phan tu sinh g
+//  int bit_size = 512;
+
+// 2. Sinh khoa rieng cua Alice va Bob
+
+// 3. Tinh gia tri cong khai cua Alice va Bob
+
+// 4. Tinh bi mat chung
+
+// 5. Hien thi ket qua va xac minh rang bi mat trung khop
+
+//}
